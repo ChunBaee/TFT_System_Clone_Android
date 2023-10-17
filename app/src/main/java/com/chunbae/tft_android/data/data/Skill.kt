7777de82fd4ccf78ba4afb9fd243dc56ad80ff25
ChunBaee <mljung10@naver.com>
2023-10-18 05:18:13 +0900

    fix: 유닛에 현재 상태를 담는 변수 추가

--- app/src/main/java/com/chunbae/tft_android/data/data/Skill.kt
+++ app/src/main/java/com/chunbae/tft_android/data/data/Skill.kt
@@ -5,7 +5,7 @@ data class Skill(
     val thumbNail : String, // 스킬에도 아이콘 이미지가 존재한다!
     val type : List<SkillType>, // 스킬이 여러가지 타입일 수 있으므로 List로 선언했다.
     val skillRange : Int?, // 스킬 범위
-    val skillTarget : Unit? // 특정 유닛 타겟일 경우를 대비 ex) 체력이 가장 낮은 아군
+    val skillTarget : Unit?, // 특정 유닛 타겟일 경우를 대비 ex) 체력이 가장 낮은 아군
     val damageType : List<DamageType> // 데미지 타입이 복합적일수도 있으므로 List로 선언했다.
 )
 
--- app/src/main/java/com/chunbae/tft_android/data/data/Unit.kt
+++ app/src/main/java/com/chunbae/tft_android/data/data/Unit.kt
@@ -3,6 +3,7 @@ package com.chunbae.tft_android.data.data
 data class Unit(
     val name : String,
     val unitCost : Int,
+    val unitStatus : UnitStatus, // 유닛의 현재 상태이다. 걸어가는 중인지, CC를 당했는지 등의 상태!
     val thumbNail : String, // 유닛의 초상화 이미지이며, 서버를 통한 ImagePath를 예상하여 String으로 선언했다.
     val model : String, // 유닛의 모델링을 가정한 이미지이며, 역시 서버를 통한 ImagePath를 예상해 String으로 선언했다.
     val trait : List<Trait>, // 특성 시너지이다. 각 유닛이 최소 2개 최대 3개를 가지므로 List로 선언했다.
@@ -21,3 +22,12 @@ data class Unit(
     val activeSkill : Skill,
     val equipItem : List<Item> // 하나의 유닛은 아이템을 총 3개까지 장착할 수 있으므로 List로 선언했다.
 )
+
+enum class UnitStatus() {
+    STAND,
+    MOVE,
+    CROWN_CONTROL,
+    PROVOKE,
+    DEAD,
+    READY_REVIVE // 특정 유닛의 경우 부활하기도 한다.
+}
