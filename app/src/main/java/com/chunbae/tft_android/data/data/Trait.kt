6519bbcda51badc37659c3f4adeacf78af94fb08
ChunBaee <mljung10@naver.com>
2023-10-22 15:22:34 +0900

    feat: 안드로이드로 전략적 팀 전투 유사하게 구사해보기(2) - 특성 구현

--- app/src/main/java/com/chunbae/tft_android/data/data/Trait.kt
+++ app/src/main/java/com/chunbae/tft_android/data/data/Trait.kt
@@ -1,5 +1,8 @@
 package com.chunbae.tft_android.data.data
 
 data class Trait(
-    val name : String
+    val name : String,
+    val traitIcon : String,
+    val traitUnits : List<Unit>,
+    val traitLevel : List<Int>
 )
